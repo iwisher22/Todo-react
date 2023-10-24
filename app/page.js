9c20b9acc2514ae9d9d9d01980a4ea3aa1447495
iwisher22 <ashusingh9559299029@gmail.com>
2023-10-25 00:19:36 +0530

    sd

--- app/page.js
+++ app/page.js
@@ -13,6 +13,13 @@ const page = () => {
 		settask("");
 		console.log(setmaintask);
 	};
+
+	const deleteHandle = (i) => {
+		let copytask = [...maintask];
+		copytask.splice(i, 1);
+		setmaintask(copytask);
+	};
+
 	let renderTask = <h2>No record found</h2>;
 
 	if (maintask.length > 0) {
@@ -23,6 +30,13 @@ const page = () => {
 						<h5 className="text-2xl font-semibold">{t.task}</h5>
 						<h6 className="text-2xl font-semibold">{t.desc}</h6>
 					</div>
+					<button
+						onClick={() => {
+							deleteHandle(i);
+						}}
+						className="br-red-200 text-white px-4 py-2 rounded">
+						DELETE
+					</button>
 				</li>
 			);
 		});
